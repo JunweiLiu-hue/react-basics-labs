c425f8d8457264c238869ee73283838344584e1f
unknown <20109222@mail.wit.ie>
2024-10-01 14:29:44 +0100

    Add task: form added

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
+import AddTaskForm from './components/Form';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -26,20 +27,20 @@ function App() {
   
   return (
     <div className="container">
-      <h1>Tasky</h1>
-      {taskState.tasks.map((task, index) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      key={task.id}
-      done={task.done}
-      markDone={() => doneHandler(index)}
-      deleteTask = {() => deleteHandler(index)}
-    />
-  ))}
-  
-    </div>
+    <h1>Tasky</h1>
+    {taskState.tasks.map((task, index) => (              
+      <Task 
+        title={task.title}
+        description={task.description}
+        deadline={task.deadline}
+        key={task.id}
+        done={task.done}
+        markDone={() => doneHandler(index)}
+        deleteTask = {() => deleteHandler(index)}
+      />
+    ))}
+    <AddTaskForm />
+  </div>
   );
 }
 
